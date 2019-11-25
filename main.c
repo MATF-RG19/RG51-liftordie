e4485db425ab0e059573e4c7ccfc18ce28f02bb4
momciloknezevic7 <moma.knezevic7@gmail.com>
2019-11-25 22:41:32 +0100

    Nedelja 2

--- main.c
+++ main.c
@@ -15,48 +15,111 @@ static void on_display(void)
     
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective( 120, window_width/(float)window_height , 1 , 1000 );
+    gluPerspective( 120, window_width/(float)window_height , 1 , 100000 );
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-        0,1,25,
+        0,10,25,
         0,0,0,
         0,1,0);
     
-    //srednja linija
-    glColor3f(1,1,0.3);
-    glBegin(GL_POLYGON);
-        glVertex3f( 30,  -50,  100);
-        glVertex3f(-30,  -50,  100);
-        glVertex3f(-30,   10, -300);
-        glVertex3f( 30,   10, -300);
-    glEnd();
     
+    //Koeficijenti za osvetljenje igraca:
+    GLfloat material_ambient[]  = { 0.1, 0.7, 0.1, 1 };
+    GLfloat material_diffuse[]  = { 0.4, 0.4, 0.1, 1 };
+    GLfloat material_specular[] = { 1, 1, 1, 1 };
+    GLfloat high_shininess[]    = { 80 };
+    GLfloat no_material[]       = { 0, 0, 0, 1 };
+    
+    //Koeficijenti za osvetljenja  srednje staze:
+    GLfloat material_ambient1[]  = { 0.4, 0.4, 0.4, 1 };
+    GLfloat material_diffuse1[]  = { 0.3, 0.3, 0.3, 1 };
+    GLfloat material_specular1[] = { 1, 1, 1, 1 };
+    GLfloat high_shininess1[]    = { 100 };
+    GLfloat no_material1[]       = { 0, 0, 0, 1 };
+    
+    //Koeficijenti za osvetljenja  leve i desne staze:
+    GLfloat material_ambient2[]  = { 0.1, 0.7, 0.4, 1 };
+    GLfloat material_diffuse2[]  = { 0.9, 0.3, 0.9, 1 };
+    GLfloat material_specular2[] = { 1, 1, 1, 1 };
+    GLfloat high_shininess2[]    = { 100 };
+    GLfloat no_material2[]       = { 0, 0, 0, 1 };
+    
+    
+    //Igrac
+    glPushMatrix();
+    
+        glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);//prava boja sfere
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);//odredjuje boju svetlosti
+        glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);//isticanje materijala
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);//veliki odsjaj
+        glMaterialfv(GL_FRONT, GL_EMISSION, no_material);
+        
+        glTranslatef(0, 0, 10);
+        glColor3f(0,0.5,0.1);
+        glutWireSphere(3,350,350);
+    glPopMatrix();
+    
+    
+    //srednja linija
+    glPushMatrix();
+        
+        glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient1);//prava boja
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse1);//odredjuje boju svetlosti
+        glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular1);//isticanje materijala
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess1);//veliki odsjaj
+        glMaterialfv(GL_FRONT, GL_EMISSION, no_material1);
+        
+        
+        glColor3f(1,1,0.3);
+        glBegin(GL_POLYGON);
+            glVertex3f( 30,  -50,  100);
+            glVertex3f(-30,  -50,  100);
+            glVertex3f(-30,   10, -300000);
+            glVertex3f( 30,   10, -300000);
+        glEnd();
+        
+    glPopMatrix();
     
     //leva linija
-    glColor3f(0.5,0.7,0.5);
-    glBegin(GL_POLYGON);
-        glVertex3f(-30,  -50,  100);
-        glVertex3f(-90,  -50,  100);
-        glVertex3f(-90,   10, -300);
-        glVertex3f(-30,   10, -300);
-    glEnd();
-    
-   //desna
-    glColor3f(0.5,0.7,0.5);
-    glBegin(GL_POLYGON);
-        glVertex3f( 30,  -50,  100);
-        glVertex3f( 90,  -50,  100);
-        glVertex3f( 90,   10, -300);
-        glVertex3f( 30,   10, -300);
-    glEnd();
-    
-    
     glPushMatrix();
-        glColor3f(0,0.5,0.1);
-        glutWireSphere(3,300,300);
+        
+        glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient2);//prava boja
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse2);//odredjuje boju svetlosti
+        glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular2);//isticanje materijala
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess2);//veliki odsjaj
+        glMaterialfv(GL_FRONT, GL_EMISSION, no_material2);
+        
+        
+        glColor3f(0.5,0.7,0.5);
+        glBegin(GL_POLYGON);
+            glVertex3f(-30,  -50,  100);
+            glVertex3f(-120,  -50,  100);
+            glVertex3f(-120,   10, -300000);
+            glVertex3f(-30,   10, -300000);
+        glEnd();
     glPopMatrix();
+    
+    
+    //desna linija
+    glPushMatrix();
+    
+        glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient2);//prava boja
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse2);//odredjuje boju svetlosti
+        glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular2);//isticanje materijala
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess2);//veliki odsjaj
+        glMaterialfv(GL_FRONT, GL_EMISSION, no_material2);
+        
+        glColor3f(0.5,0.7,0.5);
+        glBegin(GL_POLYGON);
+            glVertex3f( 30,  -50,  100);
+            glVertex3f( 120,  -50,  100);
+            glVertex3f( 120,   10, -300000);
+            glVertex3f( 30,   10, -300000);
+        glEnd();
+    glPopMatrix();
+    
     
 
     glutSwapBuffers();
@@ -65,8 +128,27 @@ static void on_display(void)
 static void initialize(void)
 {
     glClearColor(0.75,0.75,0.75,0);
- 
     
+    //Ambijentalna boja
+    GLfloat light_ambient[] = { 0, 0, 0, 1 };
+    
+    //Difuzna boja
+    GLfloat light_diffuse[] = { 1, 1, 1, 1 };
+    
+    //Spekularna boja
+    GLfloat light_specular[] = { 1, 1, 1, 1 };
+    
+    //MODEL OSVETLJENJA(biram ambijentalni model)
+    GLfloat model_ambient[] = { 0.4, 0.4, 0.4, 1 };
+    
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+     
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, model_ambient);
     
     glEnable(GL_DEPTH_TEST);
 
@@ -79,15 +161,19 @@ static void on_keyboard(unsigned char key, int x, int y)
         case 27:
        	    exit(0);
             break;
+        case 'A':
         case 'a':
             //skreni levo
             break;
+        case 'D':
         case 'd':
             //skreni desno
             break;
+        case 'W':
         case 'w':
             //idi napred
             break;
+        case 'S':
         case 's':
             //idi nazad
             break;
@@ -112,14 +198,18 @@ int main(int argc,char** argv)
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
     
+    
+    initialize();
+    
+    
     glutReshapeFunc(on_reshape);
     glutKeyboardFunc(on_keyboard);
     glutDisplayFunc(on_display);
     
+
+
     glutFullScreen();
-    
-    initialize();
-    
+
     glutMainLoop();
 
     return 0;

295b0065472d7332f56f961aa9480e5aba6590bc
momciloknezevic7 <moma.knezevic7@gmail.com>
2019-12-01 21:23:04 +0100

    Nedelja 3

--- main.c
+++ main.c
@@ -2,25 +2,93 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <time.h>
 
-static int window_width , window_height;
+
+static float x_curr , y_curr , z_curr;//trenutna pozicija
+static float r;//precnik lopte
+
+
+void napravi_prepreke()
+{
+    int x_trans,y_trans,z_trans;
+    x_trans = 0;
+    y_trans = 0;
+    z_trans = 0;
+    
+    for(int i = 0 ; i < 100 ; i++)
+    {
+        int ind  = rand() % 3;
+        int ind2 = ind + 2;
+        
+        if(ind == 0)
+            x_trans = -29;
+        else if(ind == 1)
+            x_trans = 0;
+        else
+            x_trans = 29;
+        y_trans = 0;
+        z_trans -= 100;
+        
+        
+        int size = (rand() % 15) + 10;
+        
+        float r,g,b;
+        
+        //Kocka = prepreka
+        glPushMatrix();
+            glDisable(GL_LIGHTING);
+                
+            r = (float)(rand()%10)/10;
+            g = (float)(rand()%10)/10;
+            b = (float)(rand()%10)/10;
+            glColor3f(r,g,b);
+            
+            glTranslatef(x_trans,y_trans,z_trans);
+            glutSolidCube(size);
+            
+        glPopMatrix();
+        
+        
+        //Cajnik = hemija
+        if(i % 10 == 0)
+        {
+            glPushMatrix();
+                glColor3f(0,0,0);
+                
+                glTranslatef(x_trans*(-1),y_trans,z_trans);
+                glutWireTeapot(7);
+            
+            glPopMatrix();
+        }
+        
+        
+        //Torus = obrazac
+        if(i % 5 == 0 && i % 10 != 0)
+        {
+            glPushMatrix();
+                glColor3f(1,1,1);
+                
+                glRotatef(145, 1, 0, 0);
+                glTranslatef(x_trans*(-1),y_trans,z_trans);
+                glutSolidTorus(3, 5, 36, 36);
+            glPopMatrix();
+        }
+        glEnable(GL_LIGHTING);
+    }
+    
+}
+
 
 
 static void on_display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    
-    glViewport(0,0, window_width , window_height);
-    
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective( 120, window_width/(float)window_height , 1 , 100000 );
-    
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-        0,10,25,
+        0,20,25,
         0,0,0,
         0,1,0);
     
@@ -47,6 +115,8 @@ static void on_display(void)
     GLfloat no_material2[]       = { 0, 0, 0, 1 };
     
     
+    
+    
     //Igrac
     glPushMatrix();
     
@@ -56,9 +126,11 @@ static void on_display(void)
         glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);//veliki odsjaj
         glMaterialfv(GL_FRONT, GL_EMISSION, no_material);
         
-        glTranslatef(0, 0, 10);
+        glTranslatef(0, 0, 12);
         glColor3f(0,0.5,0.1);
-        glutWireSphere(3,350,350);
+        glutWireSphere(5,350,350);
+        
+
     glPopMatrix();
     
     
@@ -121,14 +193,26 @@ static void on_display(void)
     glPopMatrix();
     
     
-
+    
+    napravi_prepreke();
+    
+    
     glutSwapBuffers();
 }
+
+
+        
 
 static void initialize(void)
 {
     glClearColor(0.75,0.75,0.75,0);
     
+    //Precnik i pocetne koordinate lopte
+    r = 3;
+    x_curr = 0;
+    y_curr = 0;
+    z_curr = 10;
+
     //Ambijentalna boja
     GLfloat light_ambient[] = { 0, 0, 0, 1 };
     
@@ -154,39 +238,47 @@ static void initialize(void)
 
 }
 
+
+
+
 static void on_keyboard(unsigned char key, int x, int y)
 {
     switch(key)
     {
         case 27:
-       	    exit(0);
+            exit(0);
             break;
         case 'A':
         case 'a':
-            //skreni levo
+            //levo
             break;
         case 'D':
         case 'd':
-            //skreni desno
-            break;
-        case 'W':
-        case 'w':
-            //idi napred
+            //desno
             break;
         case 'S':
         case 's':
-            //idi nazad
+            //pocetak kretanja
+
+        default:
             break;
             
     }
     
+    
+    
 }
+
 
 static void on_reshape(int width , int height)
 {
-    window_width = width;
-    window_height = height;
+    glViewport(0,0, width , height);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective( 120, width/(float)height , 1 , 100000 );
 }
+
 
 
 int main(int argc,char** argv)
@@ -201,12 +293,10 @@ int main(int argc,char** argv)
     
     initialize();
     
-    
-    glutReshapeFunc(on_reshape);
     glutKeyboardFunc(on_keyboard);
+    glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
     
-
 
     glutFullScreen();
 
@@ -214,3 +304,4 @@ int main(int argc,char** argv)
 
     return 0;
 }
+
